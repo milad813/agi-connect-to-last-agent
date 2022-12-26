6944e3ead7ab8a3081e2ee60cf297d3e50201109
Milad Paykar <66280336+milad813@users.noreply.github.com>
2022-12-26 16:46:35 +0330

    second release
    it now supports two-way direction

--- agi-last-agent.php
+++ agi-last-agent.php
@@ -22,46 +22,49 @@ $mysql_host='localhost'
 		}
 		
 		$agi->Verbose('CONNECTED ***');
-		$query_customer="select * from cdr where src LIKE '%".$cid. "%' and disposition='ANSWERED' and lastapp='Dial' and calldate >= CURDATE()-2 order by calldate desc limit 1";
-		//$query_agent="select * from cdr where dst LIKE '%".$cid. "%' and disposition IN ('ANSWERED','NO ANSWER') and lastapp='Dial' and calldate >= CURDATE()-2 order by calldate desc limit 1";
+		$query_agent="select * from cdr where dst LIKE '%".$cid. "%' and disposition IN ('ANSWERED','NO ANSWER','BUSY') and lastapp='Dial' and calldate >= CURDATE()-2 order by calldate desc limit 1";
+		$name_agent = mysql_db_query("asteriskcdrdb",$query_agent,$con);
+		$row_agent = mysql_fetch_array($name_agent);
+		$lastDialedAgent_agent= $row_agent[src]; // last agent that called customer
+		$agi->Verbose('#####lastDialedCustomer:'.$lastDialedAgent_agent);
+		if($lastDialedAgent_agent){
+			$agi->Verbose('#####if verbosed:');
+			$agi->exec_goto('from-internal-additional', $lastDialedAgent_agent, '1');
+		}else{
+			$query_customer="select * from cdr where src LIKE '%".$cid. "%' and disposition='ANSWERED' and lastapp='Dial' and calldate >= CURDATE()-2 order by calldate desc limit 1";
+			$name_customer = mysql_db_query("asteriskcdrdb",$query_customer,$con);
+			$row_customer = mysql_fetch_array($name_customer);
+			$lastDialedAgent_customer= $row_customer[dst]; // last agent that answered customer's phone
+			$agi->Verbose('#####lastDialedAgent:'.$lastDialedAgent_customer);
+			if($lastDialedAgent_customer){
+				$agi->Verbose('#####else if verbosed:');
+				//$agi->answer();
+				//$confirmtemp = $agi->get_data('custom/call-last-agent',15000,1);
+				//$confirm = $confirmtemp['result'];
+			
+				  //if($confirm == '1'){
+				$agi->exec_goto('from-internal-additional', $lastDialedAgent_customer, '1');
 	
-		$name_customer = mysql_db_query("asteriskcdrdb",$query_customer,$con);
-		//$name_agent = mysql_db_query("asteriskcdrdb",$query_agent,$con);
-
-		$agi->Verbose('####################query:'.$query_customer);
-		
-		$row_customer = mysql_fetch_array($name_customer);
-		$lastDialedAgent_customer= $row_customer[dst];
-		
-		//$row_agent = mysql_fetch_array($name_agent);
-		//$lastDialedAgent_agent= $row_agent[cnum];
-
-		mysql_close($con);
-		
-		$agi->Verbose('#####lastDialedAgent:'.$lastDialedAgent_customer);
-		//$agi->Verbose('#####lastDialedAgent:'.$lastDialedAgent_agent);
-		
-		//$agi->Verbose('####################query:'substr($lastDialedAgent,0,7));
-
-		if($lastDialedAgent_customer){
+				//}else{
+					//$agi->exec_goto('from-internal-additional', '600', '1');	
+				//}
+	
+			}else{
+				if($lastDialedAgent_customer){
+			$agi->Verbose('#####else if verbosed:');
 			//$agi->answer();
 			//$confirmtemp = $agi->get_data('custom/call-last-agent',15000,1);
 			//$confirm = $confirmtemp['result'];
 		
   			//if($confirm == '1'){
-				$agi->exec_goto('from-internal-additional', $lastDialedAgent_customer, '1');
+			$agi->exec_goto('from-internal-additional', $lastDialedAgent_customer, '1');
 
 			//}else{
 				//$agi->exec_goto('from-internal-additional', '600', '1');	
 			//}
 
-		}else{
-			// if($lastDialedAgent_agent){
-				// $agi->exec_goto('from-internal-additional', $lastDialedAgent_agent, '1');
-			// }
-			// else {		
-					$agi->exec_goto('from-internal-additional', '901', '1');	
-				//}
 		}
-  
+			}
+		}
+		mysql_close($con);
 ?>
